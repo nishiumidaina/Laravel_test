b24896f159c43f55e45634c01d5bf8582b64e219
nishiumidaina <c0p31069@bunkyo.ac.jp>
2022-06-15 14:03:08 +0900

    'message'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -78,7 +78,7 @@ class HomeController extends Controller
             return redirect()->route('home')->with('success', '処理中です');
         }else if ($spots[0]["status"]=="None"){
            Spot::where('id', $id)->update(['status'=>'Start']);
-           $command = 'python Python/yolov5/start.py > /dev/null &';
+           $command = 'python Python/p.py > /dev/null &';
            #popen('start "" ' . $command, 'r');
            exec($command);
            return redirect()->route('home')->with('success', '処理を開始します');
